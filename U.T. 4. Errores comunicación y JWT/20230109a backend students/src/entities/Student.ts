38d268e41d382b81d09ed057919f7d64a5846614
adolfo-martin <adolfo.martin@iesramonarcas.es>
2023-01-11 12:18:25 +0100

    backend

--- U.T. 4. Errores comunicación y JWT/20230109a backend students/src/entities/Student.ts
+++
@@ -1,7 +0,0 @@
-export default class Student {
-    constructor(
-        public readonly uuid: string,
-        public readonly firstname: string,
-        public readonly lastname: string
-    ) { }
-}
